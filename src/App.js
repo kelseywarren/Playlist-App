85ab3ae5af457ef0a08947d6b038eccb7e29d0de
kelseywarren <warrenkelsey@outlook.com>
2024-04-18 18:50:55 -0500

    basic search bar component added. No attributes or styling yet.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
     <div className="App">
        <h1>The Lounge</h1>
+       <SearchBar />
     </div>
   );
 }
