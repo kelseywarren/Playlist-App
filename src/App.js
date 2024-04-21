7f2ed4dfd2710e9e6eb53f2f0d47535a132f468c
kelseywarren <warrenkelsey@outlook.com>
2024-04-21 11:12:31 -0500

    added search result component, basic layout, no attributes, no styling yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './Search/SearchBar';
+import ResultList from './SearchResults/results';
 
 function App() {
   return (
     <div className="App">
-       <h1>The Lounge</h1>
+       <h1 className="title">The Lounge</h1>
        <SearchBar />
+       <ResultList />
     </div>
   );
 }
---
+++ src/SearchResults/results.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './results-style.css';
+
+function ResultList() {
+
+    return (
+        <div className="result-container">
+            <div className="list">
+                <h2>Results</h2>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+                <div className="listItem">placeholder text</div>
+         
+            </div>
+        </div>
+    )
+};
+
+export default ResultList;
