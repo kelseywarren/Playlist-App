cc242fde2a1e4cd3f8408e86836322eb95d391cc
kelseywarren <warrenkelsey@outlook.com>
2024-04-21 11:22:09 -0500

    fixing searchbar file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import SearchBar from './Search/searchBar';
 import ResultList from './searchResults/results';
-import SearchBar from './Search/searchBar';
 
 function App() {
   return (
