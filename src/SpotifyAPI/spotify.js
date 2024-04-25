573c0b5f8c6c9b4c1761c71181f8c009f4ef3fcc
kelseywarren <warrenkelsey@outlook.com>
2024-04-24 21:12:17 -0500

    new file for API use later

---
+++ src/SpotifyAPI/spotify.js
@@ -0,0 +1,13 @@
+let accessToken;
+const clientID = 'b76671d1d5d84f47b7dd4a1a74ba9447';
+const clientSecret = 'f07b5cf6a8c14225918b635353363a79';
+const redirectURL = 'http://localhost:3000';
+
+
+
+
+
+
+
+
+export { Spotify }; 
