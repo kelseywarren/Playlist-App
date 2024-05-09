eab45d197a5100d8c7c4528fb896f5559efb6a9e
kelseywarren <warrenkelsey@outlook.com>
2024-05-09 18:25:41 -0500

    Add userProfile setup for api data

--- src/utility/spotify.js
+++ src/utility/spotify.js
@@ -84,9 +84,31 @@ const redirectURI = "http://localhost:3000";
                 }
               );
             });
-        });
+        }); 
     },
+
+    userProfile() {
+      const accessToken = Spotify.getToken();
+      const header = { Authorization: `Bearer ${accessToken}` };
+      return fetch(
+        "https://api.spotify.com/v1/me",
+        {
+          headers: header,
+          method: "POST",
+        }
+          .then((response) => response.json())
+          .then((data) => {
+            return data.map((u) => ({
+              userName: u.display_name,
+              userImg: u.images[0].url,
+            }));
+          }),
+      );
+    },
+
+    
   };
+
    
 
   Spotify.getToken(); //first search works when getToken is called outside of Spotify
@@ -98,29 +120,3 @@ const redirectURI = "http://localhost:3000";
 
 
 
-
-
-
-
-
-
-
-
-
-/* ---BASELINE-----
-fetch("https://accounts.spotify.com/api/token", {
-  body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  method: "POST"
-})
-
-.then((response) => console.log(response.json()
-.then((data) => { 
-    console.log(data) 
-    accessToken = data.access_token; 
-    console.log(accessToken)
-} 
-)));
-*/
